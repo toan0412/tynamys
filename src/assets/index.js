83392f9b337415f2ec34bbac8a6a4f39a93e81b5
toan0412 <truongtoan00189@gmail.com>
2024-04-09 10:03:54 +0700

    header-form ui final

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,9 @@
 const images = {
     logo: require('~/assets/images/logo.svg').default,
+    bell: require('~/assets/images/bell.svg').default,
+    creditCard: require('~/assets/images/creditCard.svg').default,
+    graduate: require('~/assets/images/graduate.svg').default,
+    question: require('~/assets/images/question.svg').default,
+    search: require('~/assets/images/search.svg').default,
 };
 export default images;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -4,6 +4,8 @@ import styles from './Header.module.scss';
 import images from '~/assets';
 import { DownOutlined } from '@ant-design/icons';
 import { Dropdown, Space, Divider, Button, theme } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAdd } from '@fortawesome/free-solid-svg-icons';
 const { useToken } = theme;
 
 const cx = classNames.bind(styles);
@@ -12,9 +14,14 @@ const items = [
     {
         key: '1',
         label: (
-            <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-                1st menu item
-            </a>
+            <div className={cx('workspace-dropdown-item')}>
+                <img
+                    className={cx('logo-company')}
+                    src="https://ui-avatars.com/api/?size=256&fontSize=0.5&length=2&name=a&rounded=false&bold=false&background=00897b&color=FFFFFF&uppercase=true&format=png"
+                    alt="logo company"
+                />
+                <span>adu a toàn</span>
+            </div>
         ),
     },
     {
@@ -48,6 +55,7 @@ function Header() {
                         menu={{
                             items,
                         }}
+                        trigger={['click']}
                         dropdownRender={(menu) => (
                             <div style={contentStyle}>
                                 {React.cloneElement(menu, {
@@ -60,10 +68,13 @@ function Header() {
                                 />
                                 <Space
                                     style={{
-                                        padding: 8,
+                                        padding: 4,
                                     }}
                                 >
-                                    <Button type="primary">Click me!</Button>
+                                    <Button type="primary" ghost>
+                                        <FontAwesomeIcon icon={faAdd} />
+                                        <span>Tạo không gian làm việc</span>
+                                    </Button>
                                 </Space>
                             </div>
                         )}
@@ -75,9 +86,34 @@ function Header() {
                             </Space>
                         </a>
                     </Dropdown>
+                    <div className={cx('add-quick-card-btn')}>
+                        <Button type="primary">Tạo nhanh</Button>
+                    </div>
                 </div>
 
-                <div className={cx('action')}></div>
+                <div className={cx('action')}>
+                    <div className={cx('action-item')}>
+                        <img src={images.search} alt="search" />
+                    </div>
+                    <div className={cx('action-item')}>
+                        <img src={images.graduate} alt="graduate" />
+                    </div>
+                    <div className={cx('action-item')}>
+                        <img src={images.bell} alt="bell" />
+                    </div>
+                    <div className={cx('action-item')}>
+                        <img src={images.question} alt="question" />
+                    </div>
+                    <div className={cx('action-item')}>
+                        <img src={images.creditCard} alt="creditCard" />
+                    </div>
+                    <div className={cx('avatar')}>
+                        <img
+                            src="https://lh3.googleusercontent.com/a/ACg8ocIp6zIFl2G32WoIaZW3jYyo3ge7KLKLzW7FuCTFqufS5XQ4F4Sy=s96-c"
+                            alt="avatar"
+                        />
+                    </div>
+                </div>
             </div>
         </header>
     );
